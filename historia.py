e48a403f571f5fa9d967bbc552d5440f8aad36d8
ceGarcia23 <70037454+ceGarcia23@users.noreply.github.com>
2021-06-29 23:18:52 -0500

    subiendo los primero archivos

---
+++ historia.py
@@ -0,0 +1,12 @@
+print("Hola tengo 30 años")
+
+
+print("realizando prueba")
+
+print(" realizando  tercera prueba")
+
+print("cuarto commit")
+
+print("Nuevo cambio")
+
+print("su vida y obra maestra")
---
+++ tarea.py
@@ -0,0 +1,10 @@
+print("Hola mundo")
+print("Hola mundo")
+print("Hola mundo")
+
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
